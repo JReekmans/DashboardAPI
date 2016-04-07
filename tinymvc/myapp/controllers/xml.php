69a36a1ca320611b1dbebfbe7c715e4cfaf5a3d2
JReekmans <jeroenreekmans93@hotmail.com>
2016-04-07 15:23:49 +0200

    Enabling API URL
    Example URL: http://localhost:8080/dashboardapi/index.php/xml/rebuildXML

---
+++ tinymvc/myapp/controllers/xml.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * xml.php
+ *
+ * API for editing XML file for dashboard
+ *
+ * @author		Jeroen Reekmans
+ * @company     Xerox
+ * @client      Baloise
+ * @copyright   2016
+ * @version     v0.1
+ */
+
+class Xml_Controller extends TinyMVC_Controller
+{
+  function index()
+  {
+    
+  }
+  
+  function rebuildXML()
+  {
+    echo("test");
+  }
+}
+
+?>
