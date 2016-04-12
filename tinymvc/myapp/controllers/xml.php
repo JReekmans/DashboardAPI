99f82e715fdabd5da2ece053d517a57866e06b1b
JReekmans <jeroenreekmans93@hotmail.com>
2016-04-12 12:59:29 +0200

    XML Model creation
    Read baseline XML

--- tinymvc/myapp/controllers/xml.php
+++ tinymvc/myapp/controllers/xml.php
@@ -9,7 +9,7 @@
  * @company     Xerox
  * @client      Baloise
  * @copyright   2016
- * @version     v0.1
+ * @version     v0.2
  */
 
 class Xml_Controller extends TinyMVC_Controller
@@ -23,10 +23,14 @@ class Xml_Controller extends TinyMVC_Controller
   {
     // Init
     $this->load->model('db_model','objDb');
+    $this->load->model('xml_model','objxml');
     
     $DB_data = $this->objDb->readDb();
+    $Fixed_XML_Data = $this->objxml->readFixedXML();
     
-    var_dump($DB_data);
+    var_dump($Fixed_XML_Data);
+    
+    
   }
 }
 
---
+++ tinymvc/myapp/models/xml_model.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * xml_model.php
+ *
+ * Functions to read/write XML files
+ *
+ * @author		Jeroen Reekmans
+ * @company     Xerox
+ * @client      Baloise
+ * @copyright   2016
+ * @version     v0.1
+ */
+
+class xml_Model extends TinyMVC_Model{
+    
+    function readFixedXML()
+    {
+        $fixed_XML_position = "..\\baloise\dashboard\config_fixed.xml";
+        
+        $xml=simplexml_load_file($fixed_XML_position) or die("Error: Cannot create object");
+
+        return($xml);
+    }
+}
+
+?>
