3e4db272d9de7dbba8992ec02c1abd5326778925
JReekmans <jeroenreekmans93@hotmail.com>
2016-04-26 11:02:52 +0200

    Read fixed XML

--- tinymvc/myapp/controllers/xml.php
+++ tinymvc/myapp/controllers/xml.php
@@ -21,15 +21,18 @@ class Xml_Controller extends TinyMVC_Controller
   
   function rebuildXML()
   {
+    // Variable
+    $fixed_XML_position = "..\\baloise\dashboard\config_fixed.xml";
+    
     // Init
     $this->load->model('db_model','objDb');
     $this->load->model('xml_model','objxml');
     
     $DB_data = $this->objDb->readDb();
-    $Fixed_XML_Data = $this->objxml->readFixedXML();
+    $Fixed_XML_Data = $this->objxml->readFixedXML($fixed_XML_position);
     
     var_dump($Fixed_XML_Data);
-    
+        
     
   }
 }
--- tinymvc/myapp/models/xml_model.php
+++ tinymvc/myapp/models/xml_model.php
@@ -14,10 +14,8 @@
 
 class xml_Model extends TinyMVC_Model{
     
-    function readFixedXML()
-    {
-        $fixed_XML_position = "..\\baloise\dashboard\config_fixed.xml";
-        
+    function readFixedXML($fixed_XML_position)
+    {        
         $xml=simplexml_load_file($fixed_XML_position) or die("Error: Cannot create object");
 
         return($xml);
